6fec860ee06a5d1fa387ec0a842807fd9b63eb3d
r01 <r01@r01>
2022-02-02 01:24:03 +0200

    installed tinker & created PostsController with resources

--- database/migrations/2022_02_01_225659_create_posts_table.php
+++ database/migrations/2022_02_01_225659_create_posts_table.php
@@ -14,7 +14,9 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->id();            
+            $table->string('title');            
+            $table->mediumText();            
             $table->timestamps();
         });
     }
